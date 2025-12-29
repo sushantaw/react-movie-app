79a3197a6c02613681b94d1443fa79d16ee334ad
Sushanta Biswas <bwsushanta@gmail.com>
2025-12-29 15:41:28 +0530

    Now it works!

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return(
     <div>
     <NavBar />
-    <main  className = "main-content" >
+    <main className = "main-content" >
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/favourite' element={<Favourite />} />
